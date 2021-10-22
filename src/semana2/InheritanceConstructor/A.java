a8ff7ca775bb7f16ce779138a0c64e30ece219d2
Xiaochaoy <xiaochaoy@elpuig.xeill.net>
2021-10-22 17:19:26 +0200

    v2

---
+++ src/semana2/InheritanceConstructor/A.java
@@ -0,0 +1,19 @@
+package semana2.InheritanceConstructor;
+
+public class A {
+
+    public A() {
+        System.out.println();
+    }
+}
+
+class A2 extends A {
+
+}
+
+class MainA {
+    public static void main(String[] args) {
+        //A a = new A(4);
+        A2 a2 = new A2();
+    }
+}
